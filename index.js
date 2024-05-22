de3864a707f42b25cd863f3771bec6aceb91c25e
Yousif Abdalla <Yousif@saifbelhasagroup.com>
2024-05-22 11:26:34 +0400

    top sales section done

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ $(function(){
         items: 1
     });
 
+
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
    
     
 });
