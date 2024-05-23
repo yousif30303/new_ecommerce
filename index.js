cf59b4e16ba2cd07ea651e9015723d3dddde6797
Yousif Abdalla <Yousif@saifbelhasagroup.com>
2024-05-23 10:06:31 +0400

    special-price

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ $(function(){
         }
     });
 
+       // isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+     // filter items on button click
+     $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue});
+    })
    
     
 });
