0d6bb536e500b4633aa79d4521efba21a77261f9
Yousif Abdalla <Yousif@saifbelhasagroup.com>
2024-06-04 11:02:20 +0400

    main page is done

--- index.js
+++ index.js
@@ -33,6 +33,38 @@ $(function(){
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({ filter: filterValue});
     })
+
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    })
+
    
     
 });
