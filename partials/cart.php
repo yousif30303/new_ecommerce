fa88dd36b4aa6dc690fec9a2884e4964f634dd64
Yousif Abdalla <Yousif@saifbelhasagroup.com>
2024-07-17 16:11:02 +0400

    Display Product Cart done

--- partials/cart.php
+++ partials/cart.php
@@ -3,13 +3,19 @@
             <h5 class="font-baloo font-size-20">Shopping Cart</h5>
             <div class="row">
                 <div class="col-sm-9">
+                        <?php
+                            foreach ($product->getData('cart') as $item) :
+                            $cart = $product->getProduct($item['item_id']);
+                            $subTotal[]= array_map(function($item){
+                                      
+                        ?>
                     <div class="row border-top mt-3 py-3">
                         <div class="col-sm-2">
-                            <img src="./assets/products/1.png" style="height: 120px;" alt="" srcset="" class="img-fluid">
+                            <img src="<?php echo $item['item_image']?>" style="height: 120px;" alt="" srcset="" class="img-fluid">
                         </div>
                         <div class="col-sm-8">
-                            <h5 class="font-baloo font-size-20">Samsung Galaxy 10</h5>
-                            <small>by samsung</small>
+                            <h5 class="font-baloo font-size-20"><?php echo $item['item_name']?></h5>
+                            <small>by <?php echo $item['item_brand']?></small>
                             <div class="d-flex">
                                 <div class="rating text-warning font-size-12">
                                     <span><i class="fas fa-star"></i></span>
@@ -32,43 +38,15 @@
                         </div>
                         <div class="col-sm-2 text-right">
                             <div class="font-size-20 text-danger font-baloo">
-                                $<span class="product_price">150</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row border-top mt-3 py-3">
-                        <div class="col-sm-2">
-                            <img src="./assets/products/1.png" style="height: 120px;" alt="" srcset="" class="img-fluid">
-                        </div>
-                        <div class="col-sm-8">
-                            <h5 class="font-baloo font-size-20">Samsung Galaxy 10</h5>
-                            <small>by samsung</small>
-                            <div class="d-flex">
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <a href="#" class="px-2 font-rale font-size-14">20,534 ratings | 1000+ answered questions</a>
-                            </div>
-                            <div class="qty d-flex pt-2">
-                                <div class="d-flex font-rale w-25">
-                                    <button class="qty-up border bg-light" data-id="pro2"><i class="fas fa-angle-up"></i></button>
-                                    <input type="text" data-id="pro2"  class="qty_input border px-2 w-100 bg-light" disabled value="1" placeholder="1">
-                                    <button  class="qty-down border bg-light" data-id="pro2"><i class="fas fa-angle-down"></i></button>
-                                </div>
-                                <button type="submit" name="delete-cart-submit" class="btn font-baloo text-danger px-3 border-right">Delete</button>
-                                <button type="submit" name="wishlist-submit" class="btn font-baloo text-danger">Save for Later</button>
-                            </div>
-                        </div>
-                        <div class="col-sm-2 text-right">
-                            <div class="font-size-20 text-danger font-baloo">
                                 $<span class="product_price">152</span>
                             </div>
                         </div>
                     </div>
+                        <?php
+                            return $item['item_price'];
+                            },$cart);//closing array_map function
+                            endforeach;
+                        ?>
                 </div>
                  <!-- subtotal section-->
                  <div class="col-sm-3">
